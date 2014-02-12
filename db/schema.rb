9d74c99255b0b65223f541d6753481cac96e52fd
Siwei <sg552sg552@gmail.com>
2014-02-12 08:00:11 +0800

    added README

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -104,8 +105,8 @@ ActiveRecord::Schema.define(:version => 20120811234147) do
     t.string   "locale"
     t.string   "title"
     t.string   "custom_slug"
-    t.string   "slug"
     t.string   "menu_title"
+    t.string   "slug"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
   end
